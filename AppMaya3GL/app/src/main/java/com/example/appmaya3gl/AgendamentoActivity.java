0db40468a9b206cb1685887a6363be1491117d10
Guilherme Nunes <167255909+GuiNunes07@users.noreply.github.com>
2026-03-28 15:16:55 -0300

    APLICATIVO MAYA ATT
    Subindo com mais telas.

--- AppMaya3GL/app/src/main/java/com/example/appmaya3gl/AgendamentoActivity.java
+++ AppMaya3GL/app/src/main/java/com/example/appmaya3gl/AgendamentoActivity.java
@@ -1,12 +1,15 @@
 package com.example.appmaya3gl;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 public class AgendamentoActivity extends AppCompatActivity {
 
@@ -15,5 +18,19 @@ public class AgendamentoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_agendamento);
+        MaterialButton btnConfirmar = findViewById(R.id.btn_confirmar_agendamento);
+        btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Mostra uma mensagem rápida na tela
+                android.widget.Toast.makeText(AgendamentoActivity.this,
+                        "Agendamento realizado com sucesso!",
+                        android.widget.Toast.LENGTH_LONG).show();
+
+                // Opcional: Voltar para o Dashboard após agendar
+                finish();
+            }
+        });
+
     }
 }
---
+++ AppMaya3GL/app/src/main/java/com/example/appmaya3gl/activityExercicios.java
@@ -0,0 +1,14 @@
+package com.example.appmaya3gl;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activityExercicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Carrega o visual limpo do carrossel que acabamos de criar
+        setContentView(R.layout.activity_exercicios);
+    }
+}
